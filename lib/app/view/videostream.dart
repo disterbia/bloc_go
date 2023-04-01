3e229613c1e3145996ef42522c694c8d3193a56e
disterbia <disterbia@naver.com>
2023-04-01 15:24:34 +0900

    인생

--- lib/app/view/videostream.dart
+++ lib/app/view/videostream.dart
@@ -1,5 +1,7 @@
+import 'dart:convert';
+
 import 'package:chewie/chewie.dart';
-import 'package:firebase_storage/firebase_storage.dart';
+import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 import 'package:video_player/video_player.dart';
 
@@ -27,43 +29,67 @@ class VideoScreenPage extends StatefulWidget {
 
 class _VideoScreenState extends State<VideoScreenPage> {
   List<ChewieController> _chewieControllers = [];
-
+  Dio dio = Dio();
   @override
   void initState() {
     super.initState();
     _loadVideos();
   }
 
+  int _currentPage = 0;
+  int _currentIndex=0;
+  bool _hasMoreVideos = true;
+
+  Future<ChewieController> _initializeVideo(String downloadUrl, bool autoPlay) async {
+    VideoPlayerController videoPlayerController = VideoPlayerController.network(downloadUrl);
+    await videoPlayerController.initialize();
+
+    ChewieController chewieController = ChewieController(
+      videoPlayerController: videoPlayerController,
+      autoPlay: autoPlay,
+      looping: false,
+    );
+
+    return chewieController;
+  }
+
   Future<void> _loadVideos() async {
-    List<String> videoFiles = [
-      // Firebase Storage의 m3u8 파일 이름 목록을 입력합니다.
-      "videos/e38fc7ec-ab55-4471-8046-d6e41ca27694-39116.mp4.m3u8",
-      "videos/bc975c22-9314-4eaa-8b0d-25b0ee23f131-110734.mp4.m3u8",
-      "videos/abdc9ab9-6619-4c0b-9a03-4ddbc5109886-120739.mp4.m3u8",
-      "videos/78b72825-1770-4462-9e61-9ba97eab48fb-65560.mp4.m3u8",
-      "videos/45f922d7-ee45-4c80-9682-1974b41bb118-112957.mp4.m3u8",
-      "videos/1dbed92d-668e-48ed-8e6e-ea4e68e56f0c-73847.mp4.m3u8",
+    if (!_hasMoreVideos) return;
 
-    ];
+    List<String> videoFiles = [];
+    List<String> temp = await fetchVideosFromServer(_currentPage);
+    videoFiles.addAll(temp);
 
-    final firebaseStorage = FirebaseStorage.instance;
-    int videoIndex = 0;
-    for (String videoFile in videoFiles) {
-      String downloadUrl = await firebaseStorage.ref(videoFile).getDownloadURL();
-      print('Download URL: $downloadUrl');
-      VideoPlayerController videoPlayerController = VideoPlayerController.network(downloadUrl);
-      await videoPlayerController.initialize();
+    if (videoFiles.isEmpty) {
+      _hasMoreVideos = false;
+      return;
+    }
 
-      ChewieController chewieController = ChewieController(
-        videoPlayerController: videoPlayerController,
-        autoPlay:videoIndex == 0,
-        looping: false,
-      );
+    List<Future<ChewieController>> videoPromises = [];
 
-      setState(() {
-        _chewieControllers.add(chewieController);
-      });
-      videoIndex++;
+    for (String downloadUrl in videoFiles) {
+      videoPromises.add(_initializeVideo(downloadUrl, _currentIndex == 0));
+      _currentIndex++;
+    }
+
+    List<ChewieController> loadedChewieControllers = await Future.wait(videoPromises);
+
+    setState(() {
+      _chewieControllers.addAll(loadedChewieControllers);
+    });
+
+    _currentPage++;
+  }
+
+  Future<List<String>> fetchVideosFromServer(int page) async {
+    final response = await dio.get(
+      'http://100.100.109.207:8080/videos?page=$page',
+    );
+
+    if (response.statusCode == 200) {
+      return List<String>.from(response.data);
+    } else {
+      throw Exception('Failed to load videos');
     }
   }
 
@@ -94,6 +120,10 @@ class _VideoScreenState extends State<VideoScreenPage> {
               _chewieControllers[i].videoPlayerController.seekTo(Duration.zero);
             }
           }
+
+          if (index == _chewieControllers.length - 1 && _hasMoreVideos) {
+            _loadVideos();
+          }
         },
       ),
     );
@@ -102,9 +132,8 @@ class _VideoScreenState extends State<VideoScreenPage> {
   @override
   void dispose() {
     for (ChewieController chewieController in _chewieControllers) {
-    chewieController.dispose();
+      chewieController.dispose();
     }
     super.dispose();
-    }
+  }
 }
-
